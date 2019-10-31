7f33606947b6c8f85f69232d1d3e1594cb222c2d
Nicholas Crispino <crispinonicky@gmail.com>
2019-10-31 10:15:01 -0400

    api requests

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  // all info on a repo
+  // axios.get('https://api.github.com/repos/Orange-You-Glad-I-didn-t-say-banana/javascript-mars-rover-v2')
+
+  // collaborators URL (REQUIRES AUTH): 
+  // axios.get('https://api.github.com/repos/Orange-You-Glad-I-didn-t-say-banana/javascript-mars-rover-v2/collaborators')
+
+  // all events on the repo
+  // axios.get('https://api.github.com/repos/Orange-You-Glad-I-didn-t-say-banana/javascript-mars-rover-v2/events')
+
+  // info on all contributors (including NUMBER of contributions)
+  axios.get('https://api.github.com/repos/Orange-You-Glad-I-didn-t-say-banana/javascript-mars-rover-v2/contributors')
+
+  .then(res => {
+    let contributors = res.data
+
+    console.log(contributors)
+
+    contributors.forEach((x,i)=> {
+      console.log(x.login, 'has made', x.contributions, 'contributions')
+    })
+    
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+
   return (
     <div className="App">
       <header className="App-header">
